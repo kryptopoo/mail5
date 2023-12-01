a61481c332a239b6164da7ea396ddd962a5b796d
kryptopoo <44108463+kryptopoo@users.noreply.github.com>
2023-12-01 12:20:36 +0700

    chore/deploy-netlify (#2)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,15 @@
-import { join } from 'path'
-import type { Config } from 'tailwindcss'
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
-import { skeleton } from '@skeletonlabs/tw-plugin'
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import adapter from '@sveltejs/adapter-netlify';
 
 export default {
 	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
 	theme: {
-		extend: {},
+		extend: {}
 	},
 	plugins: [
 		forms,
@@ -18,10 +19,13 @@ export default {
 				preset: [
 					{
 						name: 'gold-nouveau',
-						enhancements: true,
-					},
-				],
-			},
-		}),
+						enhancements: true
+					}
+				]
+			}
+		})
 	],
+	kit: {
+		adapter: adapter()
+	}
 } satisfies Config;
